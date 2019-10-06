26642560faa8d6f4d7069665a755e0faa332e4f1
Dominik Janiec <dominikjaniec@gmail.com>
2019-10-07 01:42:26 +0200

    BDD-E2E: Stub executed command for 'fake build'

--- examples/bdd-e2e-tests/build.fsx
+++ examples/bdd-e2e-tests/build.fsx
@@ -8,12 +8,19 @@ open Fake.Core
 open Fake.Core.TargetOperators
 
 
+let shouldExecuteThat command =
+  Trace.logfn "Will execute: '%s'" command
+
 // *** Define Targets ***
 Target.create "Build" <| fun _ ->
   Trace.log " --- Building the tests --- "
+  "dotnet build Example.Domain"
+  |> shouldExecuteThat
 
 Target.create "Test" <| fun _ ->
   Trace.log " --- Executing tests --- "
+  "dotnet test Example.UnitTests"
+  |> shouldExecuteThat
 
 
 // *** Define Dependencies ***
